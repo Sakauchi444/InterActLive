72fb679d90473641cfd86ce15b82c29f9d00a6ab
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2023-07-08 19:35:52 +0900

    add_route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request
+import reward_list
 
 app = Flask(__name__)
 
@@ -12,6 +13,10 @@ def main_page():
         text = "こんにちは" + name + "さん"
         return render_template("page.html",text=text)
 
+@app.route("/reward", methods=["POST"])
+def rewardlist():
+    rewardlist= reward_list.main()
+    return render_template("reward.html",list = rewardlist)
 ## 実行
 if __name__ == "__main__":
     app.run(debug=True)
