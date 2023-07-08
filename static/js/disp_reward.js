85bda4b1b3d3db7143f10f256fd97c81c2dbb4aa
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2023-07-08 22:35:55 +0900

    commit2

--- static/js/disp_reward.js
+++ static/js/disp_reward.js
@@ -4,3 +4,34 @@ for (var i=0; i<list_data.length; i++){
     document.write('detail：'+list_data[i].title+'<br>');
     document.write('<br>');
 }
+
+const createItem = (item = null) => {
+    const e = document.createElement('div')
+    e.className = 'item-box'
+    if (item) {
+        const title = document.createElement('a')
+        title.className = 'item-title'
+        title.innerHTML = item.title
+        e.appendChild(title)
+
+        const price = document.createElement('a')
+        price.className = 'item-price'
+        price.innerHTML = '¥' + Number(item.price).toLocaleString()
+        e.appendChild(price)
+
+        const desc = document.createElement('a')
+        desc.className = 'item-desc'
+        desc.innerHTML = '補足：'+item.detail
+        e.appendChild(desc)
+
+        const botton = document.createElement('button')
+        botton.innerHTML = '購入'
+        e.appendChild(botton)
+    }
+}
+
+const parent = document.getElementById('table-wrapper')
+
+list_data.forEach(item => {
+    parent.appendChild(createItem(item))
+})
