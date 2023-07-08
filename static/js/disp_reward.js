417335663e9a635d2844fdd2474d5388cdc6e5f2
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2023-07-08 19:33:47 +0900

    addreward

---
+++ static/js/disp_reward.js
@@ -0,0 +1,6 @@
+for (var i=0; i<list_data.length; i++){
+    document.write('Title：'+list_data[i].title+'<br>');
+    document.write('price：'+list_data[i].price+'<br>');
+    document.write('detail：'+list_data[i].title+'<br>');
+    document.write('<br>');
+}
