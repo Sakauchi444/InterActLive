338604ba12ec9be660a9dd49ba73ab1e69ff91d6
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2023-07-08 23:26:10 +0900

    incude_file

--- static/js/disp_reward.js
+++ static/js/disp_reward.js
@@ -1,7 +1,7 @@
 for (var i=0; i<list_data.length; i++){
     document.write('Title：'+list_data[i].title+'<br>');
     document.write('price：'+list_data[i].price+'<br>');
-    document.write('detail：'+list_data[i].title+'<br>');
+    document.write('detail：'+list_data[i].detail+'<br>');
     document.write('<form method="post" action="/recieve_reward"><input type="submit" class="nes-btn is-success" value="購入"></form>');
     document.write('<br>');
 }
